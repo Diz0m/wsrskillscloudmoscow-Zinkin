1e01b13b9ba4152005f0543191e352d609dde20f
Diz0m <zdimaalkino@gmail.com>
2021-12-09 14:03:14 +0300

    14:02

---
+++ docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt update
+apt install -y apt-transport-https ca-certificates software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install -y docker-ce
+systemctl enable --now docker
